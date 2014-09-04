35a9335ac83d4e65470c4d263bfff73bdeecedc2
jperry1147 <james.perry@encraft.co.uk>
2014-09-04 14:13:11 +0100

    poio

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http')
+var http = require('http');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
